42e0b6d0978c7122ac56167b8865abc2a87e00bc
Festervand <rnfmcb@delmar.umsl.edu>
2019-02-24 12:42:18 -0600

    Created node header file

--- node.h
+++ node.h
@@ -1,3 +1,18 @@
 //Rachel Festervand 
 //Compilors, project 0
-//This is the header file for the nodes and where they are defined. 
+//This is the header file for the nodes and where they are defined.
+
+#ifndef NODE_H
+#define NODE_H 
+
+
+typedef struct node {
+ 
+  int count; 
+  struct node *left; 
+  struct node *right; 
+  char *data[]; 
+
+}node; 
+
+#endif  
