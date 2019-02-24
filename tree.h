266b0369daddf6f284809f912c8201b2e2dda469
Festervand <rnfmcb@delmar.umsl.edu>
2019-02-24 12:56:48 -0600

    Created tree.h file

--- tree.h
+++ tree.h
@@ -1,3 +1,13 @@
 //Rachel Festervand 
 //Project 0 Compilors 
-//This is the header file for tree 
+//This is the header file for tree
+
+#ifndef TREE_H 
+#define TREE_H 
+
+#include "node.h"
+
+node* buildTree(char*);  
+
+#endif 
+ 
